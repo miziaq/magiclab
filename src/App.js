ec984b84ca02c341d64bc7bcb8c00db4e6e8cff7
Blazej Misiak <blazejmisiak@gmail.com>
2020-03-16 21:56:20 +0000

    Working test (#1)
    * add components, render data on harcoded object
    
    * works, now optimize
    
    * all ok
    
    * Cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TweetList from './components/tweet-list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="header">
+        MagicLab test - Blazej Misiak
       </header>
-    </div>
+      <TweetList />
+    </main>
   );
 }
 
---
+++ src/components/tweet-list.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Tweet from './tweet';
+
+class TweetList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tweets: []
+        }
+    }
+
+    componentWillMount() {
+        setInterval(
+            () => this.updateTweets(),
+            2000
+        );
+    }
+
+    async updateTweets() {
+        const endpoint = 'https://magiclab-twitter-interview.herokuapp.com/blazej-misiak/'
+        const lastId = (this.state.tweets.length && this.state.tweets[0].id) || 0;
+        if (lastId >= 10000) {
+            fetch(endpoint + 'reset')
+                .then(() => {
+                    this.setState((state) => {
+                        return { tweets: [] }
+                    });
+                })
+                .catch((e) => {
+                    console.warn(e);
+                });
+            }
+
+        const lastTimestamp = (this.state.tweets.length && this.state.tweets[0].timeStamp) ? this.state.tweets[0].timeStamp : '';
+        fetch(endpoint + 'api?afterTime=' + lastTimestamp, { mode: 'cors' })
+            .then((res) => res.json())
+            .then((tweetArray) => {
+                if (!tweetArray.length) {
+                    return;
+                }
+
+                const indexDelta = tweetArray[0].id - lastId;
+
+                this.setState((state) => {
+                    return { tweets: tweetArray.slice(0, indexDelta).concat(state.tweets) }
+                });
+            }).catch(() => {
+                // fail silently (and emit to Sentry)
+            });
+    }
+
+    buildList() {
+        const rows = [];
+
+        this.state.tweets.forEach((tweet) => {
+            rows.push(<Tweet tweet={tweet} />);
+        });
+
+        return rows;
+    }
+
+    render() {
+        return (
+            <ol className="tweet-list">
+                {this.buildList()}
+            </ol>
+        );
+    }
+}
+
+export default TweetList;
